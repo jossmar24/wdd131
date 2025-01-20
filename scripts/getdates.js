9832f3dc96ba95ea40c291a4a73e7928e0a35e4d
jossmar24 <mar24116@byui.edu>
2025-01-20 15:38:14 -0500

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,5 +6,5 @@ const lastModify = document.querySelector("#lastModified");
 const today = new Date();
 const theLastModify = document.lastModified;
 
-year.innerHTML = `&copy${today.getFullYear()} | Josselyn Juleidy Martinez | Ecuador`;
+year.innerHTML = `&copy; ${today.getFullYear()} | Josselyn Juleidy Martinez | Ecuador`;
 lastModify.innerHTML = `<span>Last Modification: ${theLastModify}</span>`;
