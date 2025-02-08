4f0b176ec84583bb068b1b02a80caa76e323cdb1
jossmar24 <mar24116@byui.edu>
2025-02-07 22:42:21 -0500

    fix

--- scripts/place.js
+++ scripts/place.js
@@ -4,5 +4,35 @@ const lastModify = document.querySelector("#lastModify");
 const today = new Date();
 const theLastModify = document.lastModified;
 
+const temperature = -1
+const windSpeed = 120.0
+const conditions = "arid";
+
 year.innerHTML = `&copy${today.getFullYear()} | Josselyn Juleidy Martinez | Ecuador`;
 lastModify.innerHTML = `<span>Last Modification: ${theLastModify}</span>`;
+
+function calculateWindChill(temp, wind) {
+    const windChill = 13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(wind, 0.16) +
+    0.3965 * temp * Math.pow(wind, 0.16);
+
+  return Math.round(windChill * 10) / 10;
+}
+
+function updateWeather() {
+  const weatherSection = document.querySelector('.js');
+  if (weatherSection) {
+    const windChill = calculateWindChill(temperature, windSpeed);
+
+    weatherSection.innerHTML = `
+      <li>Temperature: ${temperature}°C</li>
+      <li>Conditions: ${conditions}</li>
+      <li>Wind: ${windSpeed} km/h</li>
+      <li>Wind Chill: ${windChill}°C</li>
+      `;
+  }
+}
+
+updateWeather();
+    
